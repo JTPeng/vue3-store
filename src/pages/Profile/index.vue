b3aebcba9f1c228dcabf766d05f85a4879d75e75
JTPeng <404502753@qq.com>
2019-07-17 20:01:40 +0800

    修改登录显示功能

--- src/pages/Profile/index.vue
+++ src/pages/Profile/index.vue
@@ -7,12 +7,12 @@
           <i class="iconfont icon-person"></i>
         </div>
         <div class="user-info">
-          <p class="user-info-top" v-if="!user.name">{{user._id?user.name:'登录/注册'}}</p>
+          <p class="user-info-top" v-if="!user.name">{{user.phone?user.phone:'登录/注册'}}</p>
           <p v-if="!user.phone">
             <span class="user-icon">
               <i class="iconfont icon-shouji icon-mobile"></i>
             </span>
-            <span class="icon-mobile-number">{{user._id?user.phone:` 暂无绑定手机号`}}</span>
+            <span class="icon-mobile-number">{{user.name?user.name:` 暂无绑定手机号`}}</span>
           </p>
         </div>
         <span class="arrow">
